2b18c0ea52b96d80acb47b503bb01a034a9bba3f
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2020-03-27 10:31:53 -0400

    update metrics tagging; post draft

--- lib/quantum/telemetry/metrics.ex
+++ lib/quantum/telemetry/metrics.ex
@@ -4,16 +4,16 @@ defmodule Quantum.Telemetry.Metrics do
 
   @spec handle_event([:phoenix | :request | :success, ...], any, any, any) :: :ok | {:error, any}
   def handle_event([:phoenix, :request, :success], %{duration: dur}, metadata, _config) do
-    StatsdReporter.increment("phoenix.request.success", 1, [request_path: metadata.request_path])
-    StatsdReporter.timing("phoenix.request.success", dur, [request_path: metadata.request_path])
+    StatsdReporter.increment("phoenix.request.success", 1, tags: [metadata.request_path])
+    StatsdReporter.timing("phoenix.request.success", dur, tags: [metadata.request_path])
   end
 
   def handle_event([:phoenix, :request, :failure], %{duration: dur}, metadata, _config) do
-    StatsdReporter.increment("phoenix.request.failure", 1, [request_path: metadata.request_path])
-    StatsdReporter.timing("phoenix.request.failure", dur, [request_path: metadata.request_path])
+    StatsdReporter.increment("phoenix.request.failure", 1, [metadata.request_path])
+    StatsdReporter.timing("phoenix.request.failure", dur, [metadata.request_path])
   end
 
-  # 
+  #
   #   counters: {
   #     'statsd.bad_lines_seen': 0,
   #     'statsd.packets_received': 14,
